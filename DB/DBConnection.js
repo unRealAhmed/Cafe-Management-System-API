bb8ba6c9b9dd9df0577a686f9e3e4ec372d49a62
Ahmed Sayed <therealahme@gmail.com>
2023-12-08 07:17:34 +0200

    change MySQL to Sequelize

--- DB/DBConnection.js
+++ DB/DBConnection.js
@@ -1,20 +1,43 @@
 require('dotenv').config({ path: '../config.env' })
-const mysql = require('mysql2')
+const { Sequelize } = require('sequelize')
+// const mysql = require('mysql2')
 
-const connection = mysql.createConnection({
-  port: process.env.DB_PORT,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-})
-
-connection.connect((err) => {
-  if (!err) {
-    console.log(`Database Connected Successfully..👍`);
-  } else {
-    console.log(err);
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
   }
-})
+)
 
-module.exports = connection
+async function connectDB() {
+  try {
+    await sequelize.authenticate();
+    console.log(`Database Connected Successfully..👍`);
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+connectDB()
+
+// const connection = mysql.createConnection({
+//   port: process.env.DB_PORT,
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME
+// })
+
+// connection.connect((err) => {
+//   if (!err) {
+//     console.log(`Database Connected Successfully..👍`);
+//   } else {
+//     console.log(err);
+//   }
+// })
+
+module.exports = sequelize
+
