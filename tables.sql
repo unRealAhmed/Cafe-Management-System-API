18fdd0b09b38a93a8656eb3dcc754d0de6106fb7
Ahmed Sayed <therealahme@gmail.com>
2023-12-08 07:20:23 +0200

    create password hash util

--- tables.sql
+++ tables.sql
@@ -1,17 +1,17 @@
-CREATE TABLE user (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(50) NOT NULL,
-  email VARCHAR(100) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  status ENUM('active', 'inactive') DEFAULT 'active',
-  role VARCHAR(20) ENUM('admin', 'user') DEFAULT 'user',
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+-- CREATE TABLE user (
+--   id INT PRIMARY KEY AUTO_INCREMENT,
+--   name VARCHAR(50) NOT NULL,
+--   email VARCHAR(100) UNIQUE NOT NULL,
+--   password VARCHAR(255) NOT NULL,
+--   status ENUM('active', 'inactive') DEFAULT 'active',
+--   role VARCHAR(20) ENUM('admin', 'user') DEFAULT 'user',
+--   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+-- );
 
-INSERT INTO user (name, email, password,role) 
-VALUES ('Ahmed', 'ahmed@cafe.io', 'test1234','user')
+-- INSERT INTO user (name, email, password,role) 
+-- VALUES ('Ahmed', 'ahmed@cafe.io', 'test1234','user')
 
-UPDATE user
-SET role = 'admin'
-WHERE name = 'Ahmed';
+-- UPDATE user
+-- SET role = 'admin'
+-- WHERE name = 'Ahmed';
